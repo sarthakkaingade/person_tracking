03bff95e7f704ce7defff7bcc3b4157154050b7e
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2012-12-31 19:25:17 -0500

    Added condition onforward move

--- src/navigate_perforo.cpp
+++ src/navigate_perforo.cpp
@@ -95,9 +95,12 @@ void NavigatePerFoRo::navigate(bool trueDetected)
 		} else if ((TrackedShirt.x > (0.7 * Columns)) && (TrackedPant.x > (0.7 * Columns)))	{
 			//cout<<"Turn Right"<<endl;
 			msg.command = 4;
-		} else	if ( AreaRatio < 0.1f )	{
+		} else if ( (AreaRatio < 0.1f) && (TrackedShirt.y > (0.15 * Rows)) && (TrackedShirt.y < (0.85 * Rows)) )	{
 			//cout<<"Go Front"<<endl;
 			msg.command = 1;
+//		} else if ( (AreaRatio > 0.15f) || (TrackedShirt.y < (0.15 * Rows)) )	{
+//			cout<<"Go Back"<<endl;
+//			msg.command = 2;
 		} else	{
 			//cout<<"Center"<<endl;
 			msg.command = 0;
