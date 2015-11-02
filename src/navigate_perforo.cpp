df14b825d5bca66aa016c5985659efe910a2fe11
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2015-11-02 17:00:27 -0500

    Added Node to navigate PerFoRo

---
+++ src/navigate_perforo.cpp
@@ -0,0 +1,131 @@
+#include "navigate_perforo.h"
+
+/**
+ * Navigate PerFoRo
+*/
+
+NavigatePerFoRo::NavigatePerFoRo() : 
+	it_(nh_)
+{
+	image_sub_ = it_.subscribe("/ps3_eye/image_raw", 1, &NavigatePerFoRo::ImageCallback, this);
+	tracked_shirt_sub_ = nh_.subscribe("/track_shirt/tracked_shirt", 2, &NavigatePerFoRo::TrackedShirtCallback, this);
+	tracked_pant_sub_ = nh_.subscribe("/track_pant/tracked_pant", 2, &NavigatePerFoRo::TrackedPantCallback, this);
+	mode_sub_ = nh_.subscribe("/ModePerFoRo", 1, &NavigatePerFoRo::ModeCallback, this);
+	navigate_pub_ = nh_.advertise<PerFoRoControl::NavigatePerFoRo>("/NavigatePerFoRo", 1);
+	image_pub_ = it_.advertise("/object_tracking/image_raw", 1);
+}
+
+void NavigatePerFoRo::drawArrow(Mat image, Point p, Point q, Scalar color, int arrowMagnitude=9 , int thickness=2, int line_type=8, int shift=0)
+{
+	//Draw the principle line
+	cv::line(image, p, q, color, thickness, line_type, shift);
+	const double PI = 3.141592653;
+	//compute the angle alpha
+	double angle = atan2((double)p.y-q.y, (double)p.x-q.x);
+	//compute the coordinates of the first segment
+	p.x = (int) ( q.x +  arrowMagnitude * cos(angle + PI/4));
+	p.y = (int) ( q.y +  arrowMagnitude * sin(angle + PI/4));
+	//Draw the first segment
+	line(image, p, q, color, thickness, line_type, shift);
+	//compute the coordinates of the second segment
+	p.x = (int) ( q.x +  arrowMagnitude * cos(angle - PI/4));
+	p.y = (int) ( q.y +  arrowMagnitude * sin(angle - PI/4));
+	//Draw the second segment
+	line(image, p, q, color, thickness, line_type, shift);
+}
+
+void NavigatePerFoRo::TrackedShirtCallback(const person_tracking::TrackedObject msg)
+{
+	TrackedShirt = msg;
+	shirt_updated = true;
+}
+
+void NavigatePerFoRo::TrackedPantCallback(const person_tracking::TrackedObject msg)
+{
+	TrackedPant = msg;
+	pant_updated = true;
+}
+
+void NavigatePerFoRo::ModeCallback(const PerFoRoControl::MODE msg)
+{
+	PerFoRoMode = msg.MODE;
+}
+
+void NavigatePerFoRo::ImageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+	cv_bridge::CvImagePtr cv_ptr;
+	try
+	{
+	cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+	}
+	catch (cv_bridge::Exception& e)
+	{
+	ROS_ERROR("cv_bridge exception: %s", e.what());
+	return;
+	}
+
+	frame = cv_ptr->image;
+	if (shirt_updated)	{
+		shirt.x = TrackedShirt.x;
+		shirt.y = TrackedShirt.y;
+		drawArrow(frame, cv::Point(frame.cols/2, frame.rows/2), shirt, Scalar(255,0,0));
+	} 
+	if (pant_updated)	{
+		pant.x = TrackedPant.x;
+		pant.y = TrackedPant.y;
+		drawArrow(frame, cv::Point(frame.cols/2, frame.rows/2), pant, Scalar(255,0,0));
+	}
+
+	// Output modified video stream
+	image_pub_.publish(cv_ptr->toImageMsg());
+}
+
+void NavigatePerFoRo::navigate()
+{
+	PerFoRoControl::NavigatePerFoRo msg;
+	if ((TrackedShirt.x < (0.3 * Columns)) && (TrackedPant.x < (0.3 * Columns)))	{
+		//cout<<"Turn Left"<<endl;
+		msg.command = 3;
+	} else if ((TrackedShirt.x > (0.7 * Columns)) && (TrackedPant.x > (0.7 * Columns)))	{
+		//cout<<"Turn Right"<<endl;
+		msg.command = 4;
+	} else	if ( ((TrackedShirt.area + TrackedPant.area) / AREA) < 0.25 )	{
+		//cout<<"Go Front"<<endl;
+		msg.command = 1;
+	} else	{
+		//cout<<"Center"<<endl;
+		msg.command = 0;
+	}
+	if (prevmsg != msg.command)	{
+		prevmsg = msg.command;
+		navigate_pub_.publish(msg);
+	}
+}
+
+bool NavigatePerFoRo::ifvertical()
+{
+  return (TrackedPant.x >= (TrackedShirt.x - interval) && TrackedPant.x <= (TrackedShirt.x + interval));
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "Navigate_PerFoRO");
+	NavigatePerFoRo NP;
+	ros::Rate loop_rate(10);
+	while (ros::ok())
+  	{
+		if (NP.PerFoRoMode == 0)	{
+			if (NP.shirt_updated && NP.pant_updated)	{
+				NP.shirt_updated = false;
+				NP.pant_updated = false;
+				if (NP.ifvertical())	{
+					NP.navigate();
+				}
+			}
+		}
+
+		ros::spinOnce();
+    		loop_rate.sleep();
+	}
+	return 0;
+}
---
+++ src/navigate_perforo.h
@@ -0,0 +1,60 @@
+#include <ros/ros.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <PerFoRoControl/NavigatePerFoRo.h>
+#include <PerFoRoControl/MODE.h>
+#include <person_tracking/TrackedObject.h>
+
+using namespace cv;
+using namespace std;
+
+class NavigatePerFoRo
+{
+public:
+	NavigatePerFoRo();
+
+	~NavigatePerFoRo() {}
+
+	bool shirt_updated;
+	bool pant_updated;
+	int PerFoRoMode = 0;
+
+	void 	navigate();
+	bool 	ifvertical();
+
+protected:
+	
+	/**
+	* NodeHandle is the main access point to communications with the ROS system.
+	* The first NodeHandle constructed will fully initialize this node, and the last
+	* NodeHandle destructed will close down the node.
+	*/
+
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;
+	ros::Subscriber tracked_shirt_sub_;
+	ros::Subscriber tracked_pant_sub_;
+	ros::Subscriber mode_sub_;
+	ros::Publisher navigate_pub_;
+
+	Mat frame;
+	Point shirt,pant;
+	bool selectObject;
+	int prevmsg = 2, Rows = 240, Columns = 320, AREA = Rows * Columns, interval = 50;
+	person_tracking::TrackedObject TrackedShirt, TrackedPant;
+	
+
+	void 	drawArrow(Mat image, Point p, Point q, Scalar color, int arrowMagnitude, int thickness, int line_type, int shift);
+	void	ImageCallback(const sensor_msgs::ImageConstPtr& msg);
+	void 	TrackedShirtCallback(const person_tracking::TrackedObject msg);
+	void 	TrackedPantCallback(const person_tracking::TrackedObject msg);
+	void 	ModeCallback(const PerFoRoControl::MODE msg);
+};
