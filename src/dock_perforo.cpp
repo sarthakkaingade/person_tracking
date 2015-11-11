8b102ddf886a4a59517842061085582aaf9638c4
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2015-11-11 16:49:13 -0500

    Added initial support for docking og PerFoRo

---
+++ src/dock_perforo.cpp
@@ -0,0 +1,238 @@
+#include "dock_perforo.h"
+
+/**
+ * Dock PerFoRo
+*/
+
+DockPerFoRo::DockPerFoRo() : 
+	it_(nh_)
+{
+	image_sub_ = it_.subscribe("/usb_cam/image_raw", 1, &DockPerFoRo::ImageCallback, this);
+	mode_sub_ = nh_.subscribe("/ModePerFoRo", 1, &DockPerFoRo::ModeCallback, this);
+	image_dock_pub_ = it_.advertise("/dock_perforo/image_raw", 1);
+
+	IMSHOW = true;
+	selectObject = false;
+	trackObject = -1;
+	dilation_size = 1;
+	erosion_size = 1;
+	elemDilate = getStructuringElement( MORPH_ELLIPSE, Size( 2*dilation_size + 1, 2*dilation_size+1 ), Point( dilation_size, dilation_size ) );
+	elemErode = getStructuringElement( MORPH_ELLIPSE, Size( 2*erosion_size + 1, 2*erosion_size+1 ), Point( erosion_size, erosion_size ) );
+	mColorRadius = Scalar(10,100,100,0);
+	mUpperBound = Scalar(0);
+	mLowerBound = Scalar(0);
+	cv::namedWindow(OPENCV_WINDOW);
+}
+
+void DockPerFoRo::ModeCallback(const PerFoRoControl::MODE msg)
+{
+	PerFoRoMode = msg.MODE;
+}
+
+void DockPerFoRo::ImageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+	cv_bridge::CvImagePtr cv_ptr;
+	try
+	{
+	cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+	}
+	catch (cv_bridge::Exception& e)
+	{
+	ROS_ERROR("cv_bridge exception: %s", e.what());
+	return;
+	}
+
+	frame = cv_ptr->image;
+
+	char key = (char)cvWaitKey(10);
+	if (key ==27 )	{
+		ros::requestShutdown();
+	} else if ( key =='z' )	{
+		IMSHOW = true;
+		namedWindow(OPENCV_WINDOW);
+		setMouseCallback(OPENCV_WINDOW, onMouse, NULL); 
+	} else if (key == 'x')	{
+		IMSHOW =  false;
+		cvDestroyAllWindows() ;
+		namedWindow(OPENCV_WINDOW);
+	}
+
+	if (trackObject == 1)	{
+		Mat imgHSV, binFrame, imgThresh;
+ 
+		cvtColor(frame, imgHSV, CV_BGR2HSV); 
+
+		//Get binary image using HSV threshold
+		inRange(imgHSV, mLowerBound, mUpperBound, imgThresh); 
+		
+		//Morphological operations to get smoother blobs with reduced noise                
+		dilate( imgThresh, imgThresh, elemDilate );
+		erode( imgThresh, imgThresh, elemErode ); 
+		dilate( imgThresh, imgThresh, elemDilate );                               
+		erode( imgThresh, imgThresh, elemErode );                                                       
+		morphologyEx(imgThresh, imgThresh, MORPH_OPEN, structure_elem);
+
+		// Find contours
+		imgThresh.copyTo(binFrame);
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+			      
+		/// Find contours
+		findContours( binFrame, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		vector<Point> approx;
+		for (int i = 0; i < contours.size(); i++)
+		{
+			// Approximate contours
+			approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
+			// Skip small or non-convex objects
+			if (fabs(contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))	{
+				continue;
+			} else	{
+				// Detect and label circles
+				double area = contourArea(contours[i]);
+				Rect r = boundingRect(contours[i]);
+				int radius = r.width / 2;
+				if (abs(1 - ((double)r.width / r.height)) <= 0.2 && abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2)
+					rectangle( frame, r, Scalar(255,255,255), 2, 8, 0 );
+			}
+		}
+	}
+	// Update GUI Window
+	if (IMSHOW)	{
+		imshow(OPENCV_WINDOW, frame);
+		///imshow("Binary Image with Detected Object", imgThresh);
+	}
+	//cv::waitKey(3);
+
+	// Output modified video stream
+	image_dock_pub_.publish(cv_ptr->toImageMsg());
+}
+
+void DockPerFoRo::SelectObject(int event, int x, int y)
+{
+	Mat roiHSV;
+	fflush(stdout);
+	if (selectObject)	{
+		selection.x = MIN(x, origin.x);
+		selection.y = MIN(y, origin.y);
+		selection.width = std::abs(x - origin.x);
+		selection.height = std::abs(y - origin.y);
+	}
+
+	switch(event)
+	{
+
+	case CV_EVENT_RBUTTONDOWN:
+	{
+		Mat clickFrame = frame;
+		Mat hSV;
+
+		selection = Rect(x-rectOffset,y-rectOffset,rectOffset*2,rectOffset*2);
+
+		trackObject=0;
+
+		selectCenter.x = x;
+		selectCenter.y = y;
+
+		selectCentroid = selectCenter;
+
+
+		cvtColor(clickFrame, hSV, CV_BGR2HSV);
+
+		cv::Mat roiHSV = hSV(selection);
+
+		///computes mean over roi
+		cv::Scalar hsvColor = cv::mean( roiHSV );
+		cout<<"hsv"<<hsvColor<<endl;
+
+		double minH = (hsvColor.val[0] >= mColorRadius.val[0]) ? hsvColor.val[0]-mColorRadius.val[0] : 0;
+		double maxH = (hsvColor.val[0]+mColorRadius.val[0] <= 179) ? hsvColor.val[0]+mColorRadius.val[0] : 179;
+
+		mLowerBound.val[0] = minH;
+		mUpperBound.val[0] = maxH;
+
+		mLowerBound.val[1] = hsvColor.val[1] - mColorRadius.val[1];
+		mUpperBound.val[1] = hsvColor.val[1] + mColorRadius.val[1];
+
+		mLowerBound.val[2] = hsvColor.val[2] - mColorRadius.val[2];
+		mUpperBound.val[2] = hsvColor.val[2] + mColorRadius.val[2];
+
+		mLowerBound.val[3] = 0;
+		mUpperBound.val[3] = 255;
+
+		trackObject = -1;
+		//cout<<"Bounds"<<mLowerBound.val[0]<<"  "<<mUpperBound.val[0]<<endl;
+
+		break;
+	}
+
+
+	case CV_EVENT_LBUTTONDOWN:
+	{
+		//cout<<"L BTN DWN"<<endl;
+		//drawing = true;
+		origin = Point(x,y);
+		selection = Rect(x,y,0,0);
+		selectObject = true;
+		trackObject=0;
+		//cout<<"Left B"<<origin<<endl;
+		break;
+	}
+
+	case CV_EVENT_LBUTTONUP:
+	{
+		Mat clickFrame = frame;
+		//cout<<"L BTN UP"<<endl;
+		selectObject = false;
+		if( selection.width > 0 && selection.height > 0 )
+		  trackObject = 1;
+
+		selectCenter.x = (int)(selection.x + selection.width/2);
+		selectCenter.y = (int)(selection.y + selection.height/2);
+
+		selectCentroid = selectCenter;
+
+		//defines roi
+		cv::Rect roi( selection.x, selection.y, selection.width, selection.height );
+
+		//copies input image in roi
+		cv::Mat image_roi = clickFrame(roi);
+
+		cvtColor(image_roi, roiHSV, CV_BGR2HSV);
+
+		//computes mean over roi
+		cv::Scalar hsvColor = cv::mean( roiHSV );
+		cout<<"hsv"<<hsvColor<<endl;
+
+		double minH = (hsvColor.val[0] >= mColorRadius.val[0]) ? hsvColor.val[0]-mColorRadius.val[0] : 0;
+		double maxH = (hsvColor.val[0]+mColorRadius.val[0] <= 179) ? hsvColor.val[0]+mColorRadius.val[0] : 179;
+
+		mLowerBound.val[0] = minH;
+		mUpperBound.val[0] = maxH;
+
+		mLowerBound.val[1] = hsvColor.val[1] - mColorRadius.val[1];
+		mUpperBound.val[1] = hsvColor.val[1] + mColorRadius.val[1];
+
+		mLowerBound.val[2] = hsvColor.val[2] - mColorRadius.val[2];
+		mUpperBound.val[2] = hsvColor.val[2] + mColorRadius.val[2];
+
+		mLowerBound.val[3] = 0;
+		mUpperBound.val[3] = 255;
+
+		break;
+	}
+	}
+}           
+
+void onMouse(int event, int x, int y, int, void* )
+{
+	dp::DP->SelectObject(event,x,y);
+}
+ 
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "Dock_PerFoRo");
+	dp::DP = new DockPerFoRo;
+	ros::spin();
+	return 0;
+}
---
+++ src/dock_perforo.h
@@ -0,0 +1,62 @@
+#include <ros/ros.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <PerFoRoControl/MODE.h>
+
+using namespace cv;
+using namespace std;
+
+static const std::string OPENCV_WINDOW = "Dock PerFoRo Window";
+static void onMouse(int event, int x, int y, int, void* );
+
+class DockPerFoRo
+{
+public:
+	DockPerFoRo();
+
+	~DockPerFoRo() {cv::destroyWindow(OPENCV_WINDOW);} 
+	void 	SelectObject(int event, int x, int y);
+
+protected:
+	
+	/**
+	* NodeHandle is the main access point to communications with the ROS system.
+	* The first NodeHandle constructed will fully initialize this node, and the last
+	* NodeHandle destructed will close down the node.
+	*/
+
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_dock_pub_;
+	ros::Subscriber mode_sub_;
+
+	Mat frame;
+	bool IMSHOW;
+	int trackObject;
+	int rectOffset;
+	int dilation_size, erosion_size;
+	Mat elemDilate, elemErode;
+	Mat structure_elem;
+	Rect selection;
+	Scalar mColorRadius;
+	Scalar mLowerBound;
+	Scalar mUpperBound;
+	Point selectCentroid, selectCenter, origin;
+	bool selectObject;
+	int navX, navY, prevmsg = 1, PerFoRoMode = 4;
+
+	void	ImageCallback(const sensor_msgs::ImageConstPtr& msg);
+	void 	ModeCallback(const PerFoRoControl::MODE msg);
+};
+
+namespace dp
+{
+    DockPerFoRo *DP = NULL;
+}
