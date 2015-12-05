a9569b0b4c96c6d618f030ef9bbb55b8d3a17af4
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2015-12-04 20:06:18 -0500

    Changed area ratio thresholds to reduce distance between perforo and person

--- src/navigate_perforo.cpp
+++ src/navigate_perforo.cpp
@@ -107,10 +107,10 @@ void NavigatePerFoRo::navigate(bool trueDetected)
 		} else if ((TrackedShirt.x > (0.7 * Columns)) && (TrackedPant.x > (0.7 * Columns)))	{
 			//cout<<"Turn Right"<<endl;
 			msg.command = 4;
-		} else if ( (AreaRatio < 0.07f) && (TrackedShirt.y > (0.15 * Rows)) && (TrackedShirt.y < (0.85 * Rows)) )	{
+		} else if ( (AreaRatio < 0.2f) && (TrackedShirt.y > (0.15 * Rows)) && (TrackedShirt.y < (0.85 * Rows)) )	{
 			//cout<<"Go Front"<<endl;
 			msg.command = 1;
-		} else if ( (AreaRatio > 0.15f) || (TrackedShirt.y < (0.1 * Rows)) )	{
+		} else if ( (AreaRatio > 0.35f) || (TrackedShirt.y < (0.1 * Rows)) )	{
 			cout<<"Go Back"<<endl;
 			msg.command = 2;
 		} else	{
